edb3ec73934c2c8a15d30fa14fe8ae9e86603072
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-11-25 09:42:54 +0000

    add new updates

--- check_specifiers.c
+++ check_specifiers.c
@@ -1,29 +1,50 @@
 #include "main.h"
 
-/**
-  * check_specifier - checks if a character is a valid specifier
-  *                  and assigns the appropriate function to print.
-  * @format: the specifier (char *)
-  *
-  * Return: pointer to function.
-  */
-int (*check_specifier(const char *format))(va_list)
-{
-	int i;
-	spec_t spec_array[6] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"i", print_int},
-		{"d", print_decimal},
-		{NULL, NULL}};
+/**print_specifier - Prints an argument based on its type
+ * @format: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ * Return: 1 or 2;
+ */
 
-	for (i = 0; spec_array[i].spec_id != NULL; i++)
+int print_specifier(const char *format, int *index, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i, unknown_count = 0, counts = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (format[*index] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
+	if (fmt_types[i].fmt == '\0')
 	{
-		if (*(spec_array[i].spec_id) == *format)
+		if (format[*index] == '\0')
+			return (-1);
+		unknown_count += write(1, "%%", 1);
+		if (format[*index - 1] == ' ')
+			unknown_count += write(1, " ", 1);
+		else if (width)
 		{
-			return (spec_array[i].spec_func);
+			--(*index);
+			while (format[*index] != ' ' && format[*index] != '%')
+				--(*index);
+			if (format[*index] == ' ')
+				--(*index);
+			return (1);
 		}
+		unknown_count += write(1, &format[*index], 1);
+		return (unknown_count);
 	}
-	return (NULL);
+	return (counts);
 }
